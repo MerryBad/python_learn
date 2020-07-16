e1db0d92b6f371fa4c6166ae29911cd2fb8ffd21
야옹 <49221790+MerryBad@users.noreply.github.com>
2020-07-16 15:57:03 +0900

    update 07_16

--- Day_08_02_weather.py
+++ Day_08_02_weather.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import json
 # 기상청
 url = "http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108"
 received = requests.get(url)
@@ -19,10 +20,10 @@ locations=re.findall(r'<location wl_ver="3">(.+?)</location>', received.text, re
 # print(len(locations))
 # province와 city 찾기
 # for loc in locations:
-#     # print(loc)
-#     prov = re.findall(r"<province>(.+)</province>", loc)
-#     city = re.findall(r"<city>(.+)</city>", loc)
-#     print (prov[0], city[0])
+#     print(loc)
+    # prov = re.findall(r"<province>(.+)</province>", loc)
+    # city = re.findall(r"<city>(.+)</city>", loc)
+    # print (prov[0], city[0])
 
 # data 찾기
 # <data>
@@ -34,13 +35,75 @@ locations=re.findall(r'<location wl_ver="3">(.+?)</location>', received.text, re
 # <reliability/>
 # <rnSt>30</rnSt>
 # </data>
+# for loc in locations:
+#     dates = re.findall(r'<data>(.+?)</data>', loc, re.DOTALL)
+#     for data in dates:
+#         mode=re.findall(r'<mode>(.+?)</mode>', data)
+#         tmEf=re.findall(r'<tmEf>(.+?)</tmEf>', data)
+#         wf=re.findall(r'<wf>(.+?)</wf>', data)
+#         tmn=re.findall(r'<tmn>(.+?)</tmn>', data)
+#         tmx=re.findall(r'<tmx>(.+?)</tmx>', data)
+#         rnSt=re.findall(r'<rnSt>(.+?)</rnSt>', data)
+#         print(mode[0], tmEf[0], wf[0], tmn[0], tmx[0], rnSt[0])
+
+# 서울ㆍ인천ㆍ경기도
+# A02 2020-07-19 00:00 흐림 22 27 40
+# A02 2020-07-19 12:00 흐리고 비 22 27 80
+# ==>
+# 서울ㆍ인천ㆍ경기도 A02 2020-07-19 00:00 흐림 22 27 40
+
+# for loc in locations:
+#     test = re.findall(r"<province>(.+?)</province>.+<city>(.+?)</city>", loc, re.DOTALL)
+#     # print(test[0])
+#     prov, city = test[0]
+#     dates = re.findall(r'<data>(.+?)</data>', loc, re.DOTALL)
+#     for data in dates:
+#         # result=re.findall(r'<mode>(.+?)</mode>.+<tmEf>(.+?)</tmEf>.+<wf>(.+?)</wf>.+<tmn>(.+?)</tmn>.+<tmx>(.+?)</tmx>.+<rnSt>(.+?)</rnSt>', data, re.DOTALL)
+#         # mode, tmEf, wf, tmn, tmx, rnSt = result[0]
+#         result=re.findall(r'<.+>(.+?)</.+>', data)
+#         mode, tmEf, wf, tmn, tmx, rnSt = result
+#         print(prov, city, mode, tmEf, wf, tmn, tmx, rnSt)
+
+# prov와 city를 한번에 찾기
+# for loc in locations:
+#     test = re.findall(r"<province>(.+?)</province>\r\n\t\t\t\t<city>(.+?)</city>", loc, re.DOTALL)
+#     print(test[0])
+# < province > 서울ㆍ인천ㆍ경기도 < / province >
+# < city > 서울 < / city >
+# for loc in locations:
+#     test = re.findall(r"<province>(.+?)</province>.+<city>(.+?)</city>", loc, re.DOTALL)
+#     # print(test[0])
+#     prov, city = test[0]
+#     print(prov, city)#####################다중치환######################
+#
+# 파싱한 결과를 weather.csv 파일에 저장하기
+
+# 파일로 저장하면서 , 로 구분한다
+# print(prov,city,mode,tmEf,wf,tmn,tmx,rnSt, file=f, sep=',')
+#
+# f = open("./data/weather.csv", 'w', encoding='utf-8')
+# for loc in locations:
+#     test = re.findall(r"<province>(.+?)</province>.+<city>(.+?)</city>", loc, re.DOTALL)
+#     prov, city = test[0]
+#     dates = re.findall(r'<data>(.+?)</data>', loc, re.DOTALL)
+#     for data in dates:
+#         result=re.findall(r'<.+>(.+?)</.+>', data)
+#         mode, tmEf, wf, tmn, tmx, rnSt = result
+#         f.write(prov + "," + city + ",")
+#         for i in result:
+#             f.write(i+ ",")
+#         f.write("\n")
+# f.close()
+
+f = open("./data/weather.csv", 'w', encoding='utf-8')
 for loc in locations:
+    test = re.findall(r"<province>(.+?)</province>.+<city>(.+?)</city>", loc, re.DOTALL)
+    prov, city = test[0]
     dates = re.findall(r'<data>(.+?)</data>', loc, re.DOTALL)
     for data in dates:
-        mode=re.findall(r'<mode>(.+?)</mode>', data)
-        tmEf=re.findall(r'<tmEf>(.+?)</tmEf>', data)
-        wf=re.findall(r'<wf>(.+?)</wf>', data)
-        tmn=re.findall(r'<tmn>(.+?)</tmn>', data)
-        tmx=re.findall(r'<tmx>(.+?)</tmx>', data)
-        rnSt=re.findall(r'<rnSt>(.+?)</rnSt>', data)
-        print(mode[0], tmEf[0], wf[0], tmn[0], tmx[0], rnSt[0])
+        result=re.findall(r'<.+>(.+?)</.+>', data)
+        mode, tmEf, wf, tmn, tmx, rnSt = result
+        #문자열로 변환
+        line='{},{},{},{},{},{},{},{}\n'.format(prov, city, mode, tmEf, wf, tmn, tmx, rnSt)
+        f.write(line)
+f.close()
