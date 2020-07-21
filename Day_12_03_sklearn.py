bfa866370d0b50bfd44d7a951098a343ca455516
야옹 <49221790+MerryBad@users.noreply.github.com>
2020-07-21 17:49:21 +0900

    Update 07/21

--- Day_12_03_sklearn.py
+++ Day_12_03_sklearn.py
@@ -1,8 +1,16 @@
 from sklearn import datasets, svm
-
+import numpy as np
 iris = datasets.load_iris()
 
 clf=svm.SVC()
 clf.fit(iris['data'], iris['target'])
 
 print(clf.score(iris['data'], iris['target']))
+
+y_hats=clf.predict(iris['data'])
+print(y_hats)
+
+equals=(y_hats==iris['target'])
+print(equals)
+
+print('acc : ', np.mean(equals))
